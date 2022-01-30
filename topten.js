a7b76260a9d397666178f518b0cd747243ebf2c9
sonyarugh1 <91481258+sonyarugh1@users.noreply.github.com>
2022-01-30 01:39:54 -0500

    Create topten.js

---
+++ topten.js
@@ -0,0 +1,197 @@
+var topten = function (){ document.getElementById("mydiv").style.visibility ="hidden";               document.getElementById("mydiv2").style.visibility ="hidden";
+  background(255, 255, 255);
+  fill(60, 0, 255);
+  rect(0,0,500,100);
+  fill(255,255,255);
+  textSize(45);
+  text('Top 10 list',70,70);
+  fill(0,0,0);
+  textSize(15);
+  text('I’ve composed a list of the top 10 modules I’ve studied in',10,120)
+  text('this course, not only to display what I have worked on in',10,140)
+  text('this course, but also to demonstrate my own',10,160)
+  text('understanding and opinion on each module.',10,180)
+  textSize(25)
+  text('Tap arrow to view list', 70,300)
+  textSize(35)
+  text('==>',320,380)
+  
+  
+}
+var hackathon = function(){ document.getElementById("mydiv").style.visibility ="visible";                         document.getElementById("mydiv2").style.visibility ="visible";
+document.getElementById("mydiv3").style.visibility ="visible";                         document.getElementById("mydiv4").style.visibility ="visible";
+  createCanvas(435,5250)
+  background(255, 255, 255);
+  fill(60, 0, 255);
+  rect(0,0,500,100);
+  fill(255,255,255);
+  textSize(45);
+  text('Top 10 list',70,70);
+  fill(0,0,0);
+  textSize(30);
+  text('1.Hackathon',10,140)
+  textSize(15);
+   text('Throughout this computer science course I’ve been able',10,170);
+text('Hackathon I found the hackathon module the most useful  ',10,190); 
+text('as I had to do a lot of research, and ask a lot of questions',10,210); 
+text('in order to achieve the end product. This was the largest',10,230);
+text('program I had worked on in JS so far, so it allowed me to',10,250);
+text('develop the skills necessary to create larger and more',10,270);
+text('complex programs. I was able to discuss concepts with my ',10,290);
+text('partner which made it easier to determine what was ',10,310);
+text('achievable given the timeline and we were able to execute',10,330);
+text('it together. This not only taught me about programming',10,350);
+text('and computer science but also teamwork and communication.',10,370);
+text('I have already used a large amount of what I learned in',10,390);
+text('this module throughout the rest of the course, but moving',10,410);
+text('forward I think knowing how to insert images in HTML and',10,430);
+text('hide them in certain JS files will be very useful as well',10,450);
+text('as knowing how to use scenes. One thing I do wish I had the',10,470);
+text('time to learn while coding this game was animation of objects',10,490);
+text('in JS, specifically animating the coin into the piggy bank.' , 10,510)
+textSize(30)
+text('2.Coding challenge', 10, 755)
+textSize(15);
+text('Similarly to the hackathon I found the coding challenge',10,790);
+text('extremely useful, and it is only ranked lower as I already',10,810);
+text('had the hackathon as prior knowledge and therefore did not',10,830);
+text('learn as many new concepts. Having said that, this challenge',10,850);
+text('gave me the opportunity to apply all of the concepts I ',10,870);
+text('learned in the hackathon which further reinforced them, as',10,890);
+text('well as learn a number of new concepts through research, ',10,910);
+text('questions, and working through the base code. This challenge',10,930);
+text('required a lot more problem solving than the hackathon and I ',10,950);
+text('faced a number of obstacles, but in turn I learned a lot and ',10,970);
+text('I now know how to work with buttons and create effects in ',10,990);
+text("different scenes. Although I haven't had as much time to",10,1010);
+text('apply this new found knowledge, I plan on using it anytime',10,1030);
+text('I am working with buttons and/or if statements. This',10,1050);
+text('knowledge will help me tremendously as I move forward as a ',10,1070);
+text('student, because I will not have to do as much research and ',10,1090);
+text('I can avoid certain crashes that I ran into during this ',10,1110);
+text('project. This will allow me to redirect my time and attention',10,1130);
+text('to learn new concepts and ensure that the skills I already have',10,1150);
+text('are optimized.',10,1170);
+textSize(30);
+text('3.Learn HTML',10,1430)
+textSize(15);
+text('I found the learn HTML module on codecademy very useful',10,1460);
+text('as it provided me with a strong base in HTML. I learned how',10,1480);
+text('to create a webpage using headers, titles and images, and',10,1500);
+text('I got more insight on how webpages are made. I used this ',10,1520);
+text('base knowledge  to create a large part of the webpage that',10,1540);
+text('this information is being displayed on and I will continue to ',10,1560);
+text('apply this knowledge throughout any project where I am ',10,1580);
+text('using HTML, particularly websites as that was what this ',10,1600);
+text('module focused on. This will be very useful in the future',10,1620);
+text('if I am ever trying to start my own business or if I am ',10,1640);
+text('trying to display myself to potential employers. ',10,1660);
+textSize(30);
+text('4.Learn p5.js',10,1830)
+textSize(15);
+text('The learning p5.js module on code academy was very useful',10,1860)
+text('as it gave me a proper introduction to p5.js which was ',10,1880)
+text('a web editor I used frequently in the course. This base',10,1900)
+text('knowledge made me feel a lot more comfortable when',10,1920)
+text('coding because I already knew about the web editor and all',10,1940)
+text('of this feature. I will without a doubt use this knowledge ',10,1960)
+text('in my future as a student because I will have a web editor ',10,1980)
+text('that I feel very comfortable with and will be able to ',10,2000)
+text('use under most circumstances. I also know that this editor',10,2020)
+text('is not the best for collaborative projects, so I ',10,2040)
+text('can also avoid it when working with others, which will',10,2060)
+text('save time. ',10,2080)
+textSize(30);
+text('5.Intro to js',10,2260)
+textSize(15);
+text('I found the intro to JS module on khan academy useful as ',10,2290)
+text('I was able to learn new concepts regarding JS, and',10,2310)
+text('I was able to apply this knowledge through small ',10,2330)
+text('challenges and quizzes. Although the concepts were not ',10,2350)
+text('very complex it was very helpful for me at the time as ',10,2370)
+text('JavaScript was a completely new language to me and I had no ',10,2390)
+text('prior knowledge of it which would have made it difficult ',10,2410)
+text('to jump into some of the other concepts, but after I obtained ',10,2430)
+text('this base it was much easier to grasp more difficult concepts. ',10,2450)
+text('As most of these concepts were quite simple and regularly ',10,2470)
+text('used I anticipate that I will use what I learned in this module ',10,2490)
+text('anytime that I code in JS. This will allow me to have a ',10,2510)
+text('strong base for my code and to have less bugs as I am more ',10,2530)
+text('confident in my abilities. ',10,2550)
+textSize(30);
+text('6.Data analysis, simulations',10,2760)
+text('and security',10,2790)
+textSize(15);
+text('I found this module quite interesting and it resonated with ',10,2820)
+text('me quite a bit. The data analysis and simulations resonated',10,2840)
+text('with me more on the programming side whereas the online ',10,2860)
+text('data security resonated with me more emotionally. I had ',10,2880)
+text('already done a large research project on data analysis and ',10,2900)
+text('simulations so I did not learn as much in this part of the ',10,2920)
+text('module as I have others, but I still enjoyed it as it is ',10,2940)
+text('something I am very interested in, and I am considering ',10,2960)
+text('pursuing a career in this field.  The data security ',10,2980)
+text('aspect of this module resonated with me emotionally as it ',10,3000)
+text('puts into perspective the amount of information that is ',10,3020)
+text('available about you on the internet, and the lack of real ',10,3040)
+text('privacy that you have. I will use this information moving ',10,3060)
+text('forward not only as a student but also in my everyday life to ',10,3080)
+text('ensure that my data is as safe as it can be, and I will know ',10,3100)
+text('what extra measures to take to make sure that I am as secure ',10,3120)
+text('as possible.',10,3140)
+textSize(30);
+text('7.Innovation and internet',10,3350)
+textSize(15);
+text('I found this module to be very interesting as I ',10,3380)
+text('learned about the internet and its different levels of ',10,3400)
+text('accessibility across the globe as well as how the computers ',10,3420)
+text('we know now came to be. I find this very interesting as it ',10,3440)
+text('allows us to fully appreciate the technology that we ',10,3460)
+text('have available to us and the opportunities that this ',10,3480)
+text('technology brings that may not be available to others for ',10,3500)
+text('a variety of reasons. I will use this knowledge as a student ',10,3520)
+text('to take advantage of these technologies and attempt to ',10,3540)
+text('maximize my potential and efficiency with these tools.',10,3560)
+textSize(30);
+text('8.Information and algorithms ',10,3790)
+textSize(15);
+text('I found this module very interesting as it provides us with ',10,3820)
+text('the logic behind everyday things that we may not think too  ',10,3840)
+text('much about such as music or photos. This aspect of the module  ',10,3860)
+text('was fun to think about as it gives you a deeper understanding  ',10,3880)
+text('of everything around you. I also found the algorithm aspect  ',10,3900)
+text('to be interesting as it is like a set of rules that is followed ',10,3920)
+text('over and over to achieve a certain goal. These both help me to  ',10,3940)
+text('grow as a student as they expand my general knowledge of ',10,3960)
+text('computers and data as well as the way the computer processes  ',10,3980)
+text('information. It also allows me to solve certain puzzles such ',10,4000)
+text("as the rubik's cube by using an algorithm or a set of rules." ,10,4020)
+textSize(30);
+text('9.Natural simulations ',10,4230)
+textSize(15);
+text('The natural simulations module was very helpful for my ',10,4260)
+text('development as it allowed me to practice what I learnt ',10,4280)
+text('throughout the khan academy course with a variety of projects ',10,4300)
+text('that pushed me at the time. These projects were small ',10,4320)
+text('and simple enough that I could complete them with my prior ',10,4340)
+text('knowledge, but also complex enough that they began to ',10,4360)
+text('prepare me for larger projects like the hackathon and coding',10,4380)
+text('challenge. I think this was a very important middle ground for ',10,4400)
+text('me as it connected the smaller challenges and the complex ',10,4420)
+text('programs so the gap between them was smaller. I will continue ',10,4440)
+text(' to use what I learned in this module when coding similar ',10,4460)
+text('projects or larger programs. ',10,4480)
+textSize(30);
+text('10.Computer programming ',10,4690)
+textSize(15);
+text('The general computer programming module of khan academy ',10,4720)
+text('was useful as it provided me with a good background  ',10,4740)
+text('information for everything I mentioned above which is important  ',10,4760)
+text('as the more complex concepts cannot not be applied without a ',10,4780)
+text('good base. I will say that this type of learning does not suit me ',10,4800)
+text('as well as guided lessons from a teacher, or larger programs,  ',10,4820)
+text('but it was still useful nonetheless. I will continue to use and ',10,4840)
+text('apply this knowledge not only when coding in JS but also when ',10,4860)
+text('coding in other languages because a lot of this information ',10,4880)
+text('is useful for any programming.',10,4900)
+}
