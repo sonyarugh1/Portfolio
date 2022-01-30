696799a1f4b7e80c6f55baac9c165b0355832dbe
sonyarugh1 <91481258+sonyarugh1@users.noreply.github.com>
2022-01-30 01:38:23 -0500

    Create homepage.js

---
+++ homepage.js
@@ -0,0 +1,22 @@
+var homepage = function (){
+    background(255, 255, 255);
+  fill(60, 0, 255);
+  rect(0,0,500,100);
+  fill(255,255,255);
+  textSize(45);
+  text('Homepage',70,70);
+  fill(0,0,0);
+  textSize(15);
+  text('Throughout this computer science course I’ve been able',10,120);
+  text('to expand my understanding of a number of computer ',10,140);
+  text('science concepts.I have chosen to display this new found',10,160);
+  text('understanding through this website, which I have created',10,180);
+  text('using html, css, and js. It contains a breakdown of a',10,200);
+  text('number of important aspects in this course as well as',10,220);
+  text('some of my personal opinions on different assignments',10,240);
+ text('and modules.',10,260);
+  fill(255,255,255);
+  rect(150,300,100,50);
+  fill(0);
+  text('Top 10 list',165,330);
+}
